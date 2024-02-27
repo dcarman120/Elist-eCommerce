b2882ddcc7927e39da47842d0fe10c62b2bcf4fc
dcarman120 <84048047+dcarman120@users.noreply.github.com>
2024-02-26 21:27:41 -0500

    Update 2-17 Live SQL.sql

--- SQL Queries/2-17 Live SQL.sql
+++ SQL Queries/2-17 Live SQL.sql
@@ -1,4 +1,5 @@
 --1. How many refunds were there for each month in 2021? What about each quarter and week?
+
 select date_trunc(refund_ts, month) as month, 
 	--date_trunc(refund_ts, week) as week,
 	--date_trunc(refund_ts, quarter) as quarter,
@@ -9,6 +10,7 @@ group by 1
 order by 1;
 
 --2. For each region, what’s the total number of customers and the total number of orders? Sort alphabetically by region.
+
 select region, 
   count(distinct customers.id) as customer_count, 
   count(distinct orders.id) as orders_count
@@ -21,6 +23,7 @@ group by 1
 order by 1;
 
 --3. What’s the average time to deliver for each purchase platform? 
+
 select purchase_platform, 
   round(avg(date_diff(order_status.delivery_ts, orders.purchase_ts, day)),4) as avg_time_to_deliver
 from core.orders
@@ -29,6 +32,7 @@ left join core.order_status
 group by 1;
 
 --4. What were the top 2 regions for Macbook sales in 2020? 
+
 select region, 
   round(sum(usd_price),2) as macbook_sales
 from core.orders
@@ -43,6 +47,7 @@ order by 2 desc
 limit 2;
 
 --5. For each marketing channel, what was the earliest account creation date?
+
 select marketing_channel, 
   min(created_on) as earliest_created_on, 
 from core.customers
@@ -54,6 +59,7 @@ order by 2;
 
 
 --1. For each purchase platform, return the top 3 customers by the number of purchases and order them within that platform. If there is a tie, break the tie using any order. 
+
 with customer_product_count as (
   select purchase_platform,
     customer_id,
@@ -67,6 +73,7 @@ from customer_product_count
 qualify row_number() over (partition by purchase_platform order by num_purchases desc) <= 3;
 
 --alternative without qualify
+
 with customer_product_count as (
   select purchase_platform,
     customer_id,
@@ -84,6 +91,7 @@ from ranking_cte
 where order_ranking <= 3;
 
 --2. What was the most-purchased brand in the APAC region?
+
 select case when product_name like 'Apple%' or product_name like 'Macbook%' then 'Apple'
   when product_name like 'Samsung%' then 'Samsung'
   when product_name like 'ThinkPad%' then 'ThinkPad'
@@ -101,6 +109,7 @@ order by 2 desc
 limit 1;
 
 --3. Of people who bought Apple products, which 5 customers have the top average order value, ranked from highest AOV to lowest AOV?
+
 with customer_aov as (
   select customer_id, 
 		avg(usd_price) as aov
@@ -116,6 +125,7 @@ from customer_aov
 qualify row_number() over (order by aov desc) <= 5
 
 --4. For each of these top 5 customers, which products did they buy?
+
 with customer_aov as (
   select customer_id, 
 		avg(usd_price) as aov
@@ -139,6 +149,7 @@ left join core.orders
 order by 3 asc;
 
 --5. Within each purchase platform, what are the top two marketing channels ranked by average order value?
+
 with marketing_sales as (
   select purchase_platform, 
     marketing_channel, 
